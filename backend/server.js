8bdac81c6a876512f45ed84a9a34f061a71a8aa0
Mahdialaya5 <mahdibenalaya566@gmail.com>
2024-05-19 15:39:31 +0100

    port

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectdb = require('./config/connect');
 const path = require('path');
 const app = express();
-const port =  10000 ;
+const port= process.env.PORT || 5000;
 
 require('dotenv').config({ path: '../.env' }); 
 
